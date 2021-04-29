b8dc3cd4f742e0f3cae280ee2f190abcf0244c64
shinyamatsusako <matsusakoshinya0319@gmail.com>
2021-04-29 16:50:46 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,20 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
@@ -22,5 +36,5 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)  
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
