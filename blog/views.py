4e2316e4d11fa1bc0a6a61a49307ffa8d3fd728c
shinyamatsusako <matsusakoshinya0319@gmail.com>
2021-04-29 16:46:45 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,6 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -16,7 +14,7 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -24,5 +22,5 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        form = PostForm(instance=post)  
     return render(request, 'blog/post_edit.html', {'form': form})
